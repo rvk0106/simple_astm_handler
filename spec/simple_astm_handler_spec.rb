34d0417c4f826cdb41f65a0774382ded29ddc95d
Vamsi Krishna <rvk0106@gmail.com>
2024-05-12 10:55:36 +0530

    Pending Rspec

--- spec/simple_astm_handler_spec.rb
+++ spec/simple_astm_handler_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SimpleAstmHandler do
     expect(SimpleAstmHandler::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  xit "does something useful" do
     expect(false).to eq(true)
   end
 end
